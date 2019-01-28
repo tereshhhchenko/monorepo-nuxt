f4631e6c9ad7902ccc1fa9a99dd16e2aff500755
hellodeadline <hellodeadline.sn@gmail.com>
2019-01-28 06:57:48 +0300

    build: move common configuration / styleguide to the repo root level

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  // add your custom rules here
+  rules: {}
+}
--- packages/ui-library/.eslintrc.js
+++ packages/ui-library/.eslintrc.js
@@ -1,19 +1,11 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   extends: [
     '@nuxtjs',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    '../../.eslintrc.js'
   ],
   plugins: [
     'prettier'
   ],
-  // add your custom rules here
-  rules: {}
 }
